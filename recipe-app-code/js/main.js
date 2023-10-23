91ff5554c2b4f7aa1db1657bf8a900a94534d72e
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-10-22 23:28:26 -0700

    add comment and comment out fetch

--- recipe-app-code/js/main.js
+++ recipe-app-code/js/main.js
@@ -8,10 +8,20 @@ const options = {
 	}
 };
 
+/* To save calls to the API, this block of javascript will fetch 3 recipes
+   from the API and store them to localStorage.  From then on, we can just
+   use the data in localStorage to work on developing our Detailed Recipe
+   Modal.
 
+   Uncomment the fetch block and run once to save the data to localStorage,
+   then comment it back out and start testing at const recipes = ...
+*/
+
+/*
 fetch(url, options)
 .then(response => response.json())
 .then(data => {
     localStorage.setItem("recipes", JSON.stringify(data))  // has to be stringified to be in localStorage
 })
+*/
 
