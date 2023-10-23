cab1c75d961b519e6b411adcd160ccf6b0c1a96e
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-10-22 23:26:19 -0700

    add constants for for fetch function

---
+++ recipe-app-code/js/main.js
@@ -0,0 +1,9 @@
+// Constants needed for fetching 3 recipes from the TastyAPI for 'lettuce'
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=lettuce';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f3bd08de05mshbc74d5d0d7c6b6ep11586ejsn91973fbdea76',
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+	}
+};
