6cc762ee45412d8bddeb60d577f1648aa315ec7b
tauri_s <tauristclaire@gmail.com>
2023-10-26 12:43:53 -0700

    Refactor all code pulling data into variables and loop to make recipe cards

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,13 +9,8 @@ const recipeCategory = document.querySelector(".recipe-category");
 const allRecipesContainer = document.querySelector(".recipes");
 const recipeList = document.querySelector(".recipe-list");
 
-// gather some of the needed data from the first recipe
-const title = recipes.results[0].name;
-const thumbnail = recipes.results[0].thumbnail_url;
-const cookTime = recipes.results[0].cook_time_minutes;
-
 // Constants needed for fetching 3 recipes from the TastyAPI for 'lettuce'
-const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=3&q=lettuce';
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q=lettuce';
 const options = {
 	method: 'GET',
 	headers: {
@@ -24,24 +19,41 @@ const options = {
 	}
 };
 
-/*
 const getData = async function () {
-const res = await fetch(url, options);
- const data = await res.json();
- localStorage.setItem("recipes", JSON.stringify(data));
- console.log(data);
- };
- getData();
-*/
+    const res = await fetch(url, options);
+     const data = await res.json();
+     localStorage.setItem("recipes", JSON.stringify(data.results));
+     console.log(data.results);
+};
+     
+getData();
+
 
 // get the recipes from localStorage
 const recipes = JSON.parse(localStorage.getItem("recipes")); // has to be parsed back into a js object
 
-const showRepos = function (repos) {
-    for (const repo of repos) {
-        const repoObject = document.createElement("li");
-        repoObject.classList.add("repo");
-        repoObject.innerHTML = `<h3>${repo.name}</h3>`
-        repoList.append(repoObject);
+
+const showRecipes = function (recipes) {
+    for (const key in recipes) {
+        const title = recipes[key].name;
+        const thumbnail = recipes[key].thumbnail_url;
+        const recipeObject = document.createElement("li");
+        recipeObject.classList.add("recipe-card");
+        recipeObject.innerHTML = `
+            <div class="recipe">
+                <figure>
+                    <img class="recipe-image" src="${thumbnail}" alt="food picture">
+                </figure>
+                <p class="recipe-category">dinner</p>
+                <div class="recipe-title-container">
+                    <h2 class="recipe-title">${title}</h2>
+                </div>
+                <button class="recipe-button" aria-describedby="recipe-button__desc">View Recipe</button>
+                <span id="recipe-button__desc" class="sr-only">A popout window will open with recipe details</span>
+            </div>
+      `;
+        recipeList.append(recipeObject);
     }
 };
+
+showRecipes(recipes);
