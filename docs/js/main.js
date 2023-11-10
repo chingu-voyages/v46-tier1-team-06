4340b6632eac4100609134bab6bdb491ba6cf244
tauri_s <tauristclaire@gmail.com>
2023-11-10 15:10:55 -0800

    Change recipeList variable to searchResults instead to match closer to DOM and change all references in the JS to this variable

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,7 +5,7 @@ const modalCloseButton = document.querySelector(".recipe-details__exit-button");
 const searchMessages = document.querySelector(".app-instructions");
 
 // DOM element to listen to and receive data
-const recipeList = document.querySelector("#search-results");
+const searchResults = document.querySelector("#search-results");
 
 // DOM elements to get user input from
 const searchBarInput = document.querySelector('#search-bar');
@@ -21,7 +21,7 @@ const modalInstructionsList = document.querySelector("#instructions-list");
 const landingPage = document.querySelector("#landing-page");
 const modal = document.querySelector("dialog");
 const exampleRecipesDesc = document.querySelector(".example-recipes__desc");
-//recipeList declared above also falls under this category
+//searchResults declared above also falls under this category
 
 // Global Variable to hold the recipes from the getData() function
 let recipes;
@@ -50,7 +50,7 @@ searchForm.addEventListener('submit', async function (e) {
         landingPage.classList.add("hidden");
         exampleRecipesDesc.classList.add("hidden");
     }
-    if (!recipeList.hasChildNodes()) {
+    if (!searchResults.hasChildNodes()) {
         landingPage.innerHTML = `<p class="no-results">No recipes found!</p>`;
         landingPage.classList.remove("hidden");
     }
@@ -60,10 +60,10 @@ refreshButton.addEventListener("click", () => {
     window.location.reload("Refresh");
     landingPage.classList.remove("hidden");
     exampleRecipesDesc.classList.remove("hidden");
-    recipeList.classList.add("hidden");
+    searchResults.classList.add("hidden");
 })
 
-recipeList.addEventListener("click", (e) => {
+searchResults.addEventListener("click", (e) => {
     createModal(e, recipes);
 })
 
@@ -106,8 +106,8 @@ async function getData(recipeInput) {
 
 function showRecipes(recipes) {
     // remove previous search results
-    while (recipeList.hasChildNodes()) {
-        recipeList.firstElementChild.remove();
+    while (searchResults.hasChildNodes()) {
+        searchResults.firstElementChild.remove();
     }
     for (const recipe in recipes) {
         // create recipe summary cards from fetched data
@@ -136,8 +136,8 @@ function showRecipes(recipes) {
             <button id="btid${recipeID}" class="recipe-button" aria-describedby="recipe-button__desc">View Recipe</button>
         `;
         // add to DOM
-        recipeList.append(recipeObject);
-        recipeList.classList.remove("hidden");
+        searchResults.append(recipeObject);
+        searchResults.classList.remove("hidden");
     }
 };
 
